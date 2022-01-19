57ffc9407b99b0eccfe50490478576da041ca229
Nikita Chesnokov <ya.sbsmrinov@gmail.com>
2022-01-19 05:43:33 +0500

    init commit

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	cout << "Enter numb: \t";
+	int num = 0;
+	cin >> num;
+
+	cout << "\n" << num << " is your num\n";
+
+	_getch();
+	return 0;
+}
+
+int NumIsLog3(int num) {
+
+	if (!num < 0) {
+
+		
+
+	}
+
+	// if %num% != 3^n 
+	return -1;
+}
---
+++ test1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+
+	int s = 27;
+	int a = 3;
+
+	cout << log(s) / log(a);
+
+	_getch();
+	return 0;
+}
