26cf77beb0420578a2f066c506dc73a89a36ff2b
Nikita Chesnokov <ya.sbsmrinov@gmail.com>
2022-01-19 13:50:53 +0500

    lab2

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	const double price = 1.25; 
+	const int length = 10;
+	double sum = 0;
+
+	string str;
+	do {
+
+		cout << ": ";
+		getline(cin, str);
+
+		if (str.length() > length) {
+			sum += (str.length() * price) * 2;
+			//cout << (str.length() * price) * 2 << "rub" << endl;
+		}
+		else
+		{
+			sum += str.length() * price;
+			//cout << str.length() * price << "rub" << endl;
+		}
+
+	} while (str != ".");
+
+	sum -= 1.25;
+
+
+	cout << "Total: " << sum << endl;
+	
+
+	system("pause");
+	return 0;
+}
